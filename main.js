b1356e438cd2c55ffbf4ff9d828cee90a63eabec
Frank Jairo Vera <fj.verachamorro@gmail.com>
2023-11-14 17:44:48 -0500

    Cambios listos

--- main.js
+++ main.js
@@ -2,10 +2,40 @@ $(document).ready(function () {
   var pasoActual = 1;
   var totalPasos = 5;
 
+  function actualizarFormulario() {
+    $(".step").removeClass("active");
+    $(".step-" + pasoActual).addClass("active");
+    $("#paso-actual").text(pasoActual);
+    var porcentaje = ((pasoActual - 1) * 100) / (totalPasos - 1);
+    $("#barra-progreso").css("width", porcentaje + "%");
+  }
+
+  function showToast() {
+    var toast = new bootstrap.Toast(document.getElementById("toastValidacion"));
+    toast.show();
+  }
+
+  function validarCampos() {
+    var allValid = true;
+    $(".step-" + pasoActual).find("input, select, textarea").each(function () {
+      if (!this.checkValidity()) {
+        $(this).addClass("is-invalid");
+        allValid = false;
+      } else {
+        $(this).removeClass("is-invalid");
+      }
+    });
+    return allValid;
+  }
+
   $(".siguiente").on("click", function () {
     if (pasoActual < totalPasos) {
-      pasoActual++;
-      actualizarFormulario();
+      if (validarCampos()) {
+        pasoActual++;
+        actualizarFormulario();
+      } else {
+        showToast();
+      }
     }
   });
 
@@ -15,12 +45,4 @@ $(document).ready(function () {
       actualizarFormulario();
     }
   });
-
-  function actualizarFormulario() {
-    $(".step").removeClass("active");
-    $(".step-" + pasoActual).addClass("active");
-    $("#paso-actual").text(pasoActual);
-    var porcentaje = ((pasoActual - 1) * 100) / (totalPasos - 1);
-    $("#barra-progreso").css("width", porcentaje + "%");
-  }
 });
